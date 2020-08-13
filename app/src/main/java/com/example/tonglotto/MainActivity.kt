96497766b12aedd7d512f687eddacd77e8fccd02
joontaeKim <neosigner@gmail.com>
2020-08-13 10:10:52 +0900

    20200813

--- app/src/main/java/com/example/tonglotto/MainActivity.kt
+++ app/src/main/java/com/example/tonglotto/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.tonglotto
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.TransitionDrawable
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+
     }
 }
